e47cf765d53b8bbeac14ffd22ef24625067e2a96
Mariana Pihur <mpihur@c3r4s9.42london.com>
2024-03-13 18:57:36 +0000

    backgr change

--- create_cpe_img.c
+++ create_cpe_img.c
@@ -1,35 +1,46 @@
 #include "so_long.h"
 
-void    put_pixel_img(t_data *img, int x, int y, int color)
+/*void    put_pixel_img(t_data *img, int x, int y, int color)
 {
     char    *dst;
 
+    //printf("%d, ", color);
+    //printf("Img address in put_pixel_img: %p\n", img->addr);
+    //printf("Color received in put_pixel_img: %d\n", color);
     if (color == (int)0xFF000000)
-        return ;
-    if (x >= 0 && y >= 0 && x < TILE_SIDE && y < TILE_SIDE)
     {
         dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-        *(unsigned int *)dst = color;
+        *(unsigned int *)dst = 0x2F2E2F;
+        //printf("OTHER\n");
     }
-}
+    else
+        return ;
+}*/
 
 unsigned int    get_pixel_img(t_data *img, int x, int y)
 {
     return (*(unsigned int *)((img->addr + (y * img->line_length) + (x * img->bits_per_pixel / 8))));
 }
 
-void    put_img_to_img(t_data *dst, t_data *src, int x, int y)
+void    put_img_to_img(t_data *img, int x, int y)
 {
     int i;
     int j;
+    char    *dst;
 
+//    printf("SRC address in put_img_to_img: %p\n", src->addr);
+//    printf("DST address in put_img_to_img: %p\n", dst->addr);
     i = 0;
     while (i < TILE_SIDE)
     {
         j = 0;
         while (j < TILE_SIDE)
         {
-            put_pixel_img(dst, x + i, y + j, get_pixel_img(src, i, j));
+            if (get_pixel_img(img, i, j) == (unsigned int)0xFF000000)
+            {
+                dst = img->addr + ((y + j) * img->line_length + (x + i) * (img->bits_per_pixel / 8));
+                *(unsigned int *)dst = 0x2F2E2F;
+            }
             j++;
         }
         i++;
@@ -39,14 +50,15 @@ void    put_img_to_img(t_data *dst, t_data *src, int x, int y)
 void    create_collectible(t_win *map)
 {
 	t_data	*c_img;
-    //int y;
-    //int x;
+    int y;
+    int x;
 
 	c_img = (t_data *)malloc(sizeof(t_data));
-//    y = x = 0;
+    y = x = 0;
     c_img->img = mlx_xpm_file_to_image(map->mlx_ptr, C_PATH, &(map->img_width), &(map->img_height));
 	c_img->addr = mlx_get_data_addr(c_img->img, &c_img->bits_per_pixel,
             &c_img->line_length, &c_img->endian);
+//    printf("%p\n%p\n", c_img->addr, map->dimg->addr);
+    put_img_to_img(c_img, x, y); 
     map->c_img = c_img;
-    //put_img_to_img(c_img, map->dimg, x, y); 
 }
