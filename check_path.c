73cf6dbcb0bc6e5fb3074322edfe318d20897ed0
Mariana Pihur <mpihur@c2r7s9.42london.com>
2024-03-14 19:40:48 +0000

    verify upd

--- check_path.c
+++ check_path.c
@@ -18,7 +18,10 @@ int     find_player(t_list  **temp, int i)
         while (i < (int)ft_strlen((*temp)->content) - 1)
         {
             if (ft_strncmp(&((char *)(*temp)->content)[i], "P", 1) == 0)
+            {
+                ft_printf("%s\n, %c", ((char *)(*temp)->content), ((char *)(*temp)->content)[i]);
                 return (i);
+            }
             i++;
         }
         *temp = (*temp)->next;
@@ -31,28 +34,29 @@ void    verify_head_path(t_list **temp, t_list **head, int i, t_list *prev)
     if (i == -1 || (*temp) == NULL)
         return ;
     /*moving left*/
+    prev = find_prev_node(*temp, *head);
     if (i - 1 >= 0 && ft_strncmp(&((char *)(*temp)->content)[i - 1], "1", 1) != 0)
     {
         ((char *)(*temp)->content)[i - 1] = 'V';
-        verify_head_path(temp, head, i - 1, find_prev_node(*temp, *head));
+        verify_head_path(temp, head, i - 1, NULL);
     }
     /*moving right*/
     if (i + 1 < ((int)ft_strlen((char *)(*temp)->content) - 1) && ft_strncmp(&((char *)(*temp)->content)[i + 1], "1", 1) != 0)
     {
         ((char *)(*temp)->content)[i + 1] = 'V';
-        verify_head_path(temp, head, i + 1, find_prev_node(*temp, *head));
+        verify_head_path(temp, head, i + 1, NULL);
     }
     /*moving down*/
     if ((*temp)->next != NULL && ft_strncmp(&((char *)(*temp)->next->content)[i], "1", 1) != 0)
     {
         ((char *)(*temp)->next->content)[i] = 'V';
-        verify_head_path(&((*temp)->next), head, i, find_prev_node(*temp, *head));
+        verify_head_path(&((*temp)->next), head, i, NULL);
     }
     /*moving up*/
     if (prev != NULL && ft_strncmp(&((char *)(prev->content))[i], "1", 1) != 0)
     {
         ((char *)(prev->content))[i] = 'V';
-        verify_head_path(&prev, head, i, find_prev_node(prev, *head));
+        verify_head_path(&prev, head, i, NULL);
     }
 }
 
@@ -62,17 +66,17 @@ int     check_the_path(t_list *head)
     t_list  *head_cpy;
 
     head_cpy = ft_lstnew(ft_strdup(head->content));
-    while (head->content != NULL)
+    while (head->next != NULL)
     {
         head = head->next;
         ft_lstadd_back(&head_cpy, ft_lstnew(ft_strdup(head->content)));
     }
     temp = head_cpy;
-    verify_head_path(&temp, &head_cpy, find_player(&temp, 0), find_prev_node(temp, head_cpy));
-    while (head != NULL)
+    verify_head_path(&temp, &head_cpy, find_player(&temp, 0), NULL);
+    while (head_cpy != NULL)
     {
-        ft_printf("%d\n", head->content);
-        head = head->next;
+        ft_printf("%s", head_cpy->content);
+        head_cpy = head_cpy->next;
     }
     /*CHANGE!!*/
     return (1);
