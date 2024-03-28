7874a5c621b0a752038be65d2a8c66d2d6969275
Mariana Pihur <mpihur@c2r7s1.42london.com>
2024-03-28 15:13:10 +0000

    check coll norm

--- check_collectibles.c
+++ check_collectibles.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_collectibles.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpihur <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 15:12:56 by mpihur            #+#    #+#             */
+/*   Updated: 2024/03/28 15:12:57 by mpihur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 /*void    print_the_head(t_list   *head)
@@ -9,72 +21,54 @@
     }
 }*/
 
-void    find_and_put_exit(t_win *map, int i, t_list *head, int count)
+void	find_and_put_exit(t_win *map, int i, t_list *head, int count)
 {
-    int j;
+	int		j;
 
-    j = 0;    
-    while (head != NULL)
-    {
-        while (i < (int)ft_strlen((head)->content) - 1)
-        {
-            if (ft_strncmp(&((char *)(head)->content)[i], "E", 1) == 0)
-            {
-                head = *(map->head_ptr);
-                while (j++ != count)
-                    head = head->next;
-                ((char *)head->content)[i] = 'E';
-                return ;
-            }
-            i++;
-        }
-        count++;
-        head = head->next;
-        i = 0;
-    }
+	j = 0;
+	while (head != NULL)
+	{
+		while (i < (int)ft_strlen((head)->content) - 1)
+		{
+			if (ft_strncmp(&((char *)(head)->content)[i], "E", 1) == 0)
+			{
+				head = *(map->head_ptr);
+				while (j++ != count)
+					head = head->next;
+				((char *)head->content)[i] = 'E';
+				return ;
+			}
+			i++;
+		}
+		count++;
+		head = head->next;
+		i = 0;
+	}
 }
 
-/*static char    *find_pointer_to_exit(t_list *head, int i)
+int	exit_or_noexit(char *tile, t_win *map)
 {
-    while (head != NULL)
-    {
-        while (i < (int)ft_strlen((head)->content) - 1)
-        {
-            if (ft_strncmp(&((char *)(head)->content)[i], "E", 1) == 0)
-                return ((((char *)head->content) + (i * sizeof((char *)head->content)[i])));
-            //ft_printf("i=%d, ", i);
-            i++;
-        }
-        head = head->next;
-        i = 0;
-    }
-    return (NULL);
-}
-*/
-int exit_or_noexit(char *tile, t_win *map)
-{
-    //check if tile is the same as the exit location
-    if (check_collectibles(*(map->head_ptr), 0, 0) == 0)
-        find_and_put_exit(map, 0, map->exit_cpy, 0);
-    else
-        return (-1);
-    if (ft_strncmp(tile, "E", 1) != 0)
-        return (-1);
-    return (0); 
+	if (check_collectibles(*(map->head_ptr), 0, 0) == 0)
+		find_and_put_exit(map, 0, map->exit_cpy, 0);
+	else
+		return (-1);
+	if (ft_strncmp(tile, "E", 1) != 0)
+		return (-1);
+	return (0);
 }
 
-int check_collectibles(t_list *head, int i, int count)
+int	check_collectibles(t_list *head, int i, int count)
 {
-    while (head != NULL)
-    {
-        while (i < (int)ft_strlen((char *)head->content) - 1)
-        {
-            if (ft_strncmp(&((char *)head->content)[i], "C", 1) == 0)
-                count++;
-            i++;
-        }
-        i = 0;
-        head = head->next;
-    }
-    return (count);
+	while (head != NULL)
+	{
+		while (i < (int)ft_strlen((char *)head->content) - 1)
+		{
+			if (ft_strncmp(&((char *)head->content)[i], "C", 1) == 0)
+				count++;
+			i++;
+		}
+		i = 0;
+		head = head->next;
+	}
+	return (count);
 }
